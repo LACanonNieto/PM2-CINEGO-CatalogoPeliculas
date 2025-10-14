e7fc4caf88ecc688b57217f92874e4398f7fcc29
Luz Canon <luzadca@hotmail.com>
2025-10-13 21:00:49 -0500

    Asincronismo

--- front/scripts/getMovies.js
+++
@@ -1,9 +0,0 @@
-const renderMovies = require("./renderMovies");
-
-function getMovies() {
-$.get("https://students-api.up.railway.app/movies", function(data) {
-    renderMovies(data)
-    });
-}
-
-module.exports = getMovies;
--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,3 +1,38 @@
-const getMovies = require("./getMovies");
+const renderMovies = require("./renderMovies");
+const axios = require("axios");
 
-getMovies()
+// function getMovies() {
+// $.get("https://students-api.up.railway.app/movies", function(data) {
+//     renderMovies(data)
+//     });
+// }
+
+
+////////////////Con handler////////////////////////////
+// function getMovies(){
+//     const promise = axios.get("https://students-api.up.railway.app/movies");
+
+//     promise.then((respuesta) => {
+//         const movies = respuesta.data;
+//         renderMovies(movies);
+//     })
+//     .catch ((error) => {
+//         console.error("Error al obtener las peliculas:", error.message);
+//     });
+
+// }
+
+///////////////////Con Async///////////////////////////
+
+async function getMovies() {
+    try{
+        const respuesta = await axios.get("https://students-api.up.railway.app/movies");
+        const movies = respuesta.data;
+        renderMovies(movies);
+    } catch (error){
+        console.error("Error al obtener las peliculas:", error.message);
+    }
+}
+
+
+getMovies();
